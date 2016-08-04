1e8d549833427759e2266029295c738cd4194192
Zhengchao Tian <zhengchaotian@gmail.com>
2016-08-04 17:21:02 -0400

    Add project files.

---
+++ BaGa/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FontAwesome.WPF;
+using MahApps.Metro.Controls;
+
+namespace BaGa
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Icon = ImageAwesome.CreateImageSource(FontAwesomeIcon.Bolt, Brushes.White);
+        }
+    }
+}
---
+++ BaGa/ViewModel/MainViewModel.cs
@@ -0,0 +1,204 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Windows.Controls;
+using Microsoft.Win32;
+
+namespace BaGa.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        const uint WM_KEYDOWN = 0x100;
+        const uint WM_KEYUP = 0x0101;
+
+        public string ProcessName { set; get; }
+        private Process[] processList;
+
+        private string settingFilePath;
+
+        public string SettingFilePath
+        {
+            get { return settingFilePath;}
+            set
+            {
+                settingFilePath = value;
+                RaisePropertyChanged("SettingFilePath");
+            }
+        }
+        public RelayCommand StartCommand { get; set; }
+        public RelayCommand StopCommand { get; set; }
+        public RelayCommand LoadSettingCommand { set; get; }
+
+        private Dictionary<string, IntPtr> keyDictionary;
+        private List<IntPtr> validKeyList;
+
+        private BackgroundWorker worker;
+
+        private Boolean runningFlag;
+
+
+        public MainViewModel()
+        {
+            keyDictionary = new Dictionary<string, IntPtr>();
+            keyDictionary.Add("1", (IntPtr)(Keys.D1));
+            keyDictionary.Add("2", (IntPtr)(Keys.D2));
+            keyDictionary.Add("3", (IntPtr)(Keys.D3));
+            keyDictionary.Add("4", (IntPtr)(Keys.D4));
+            keyDictionary.Add("5", (IntPtr)(Keys.D5));
+            keyDictionary.Add("6", (IntPtr)(Keys.D6));
+            keyDictionary.Add("7", (IntPtr)(Keys.D7));
+            keyDictionary.Add("8", (IntPtr)(Keys.D8));
+            keyDictionary.Add("9", (IntPtr)(Keys.D9));
+            keyDictionary.Add("0", (IntPtr)(Keys.D0));
+            keyDictionary.Add("-", (IntPtr)(Keys.OemMinus));
+            keyDictionary.Add("=", (IntPtr)(Keys.Oemplus));
+            StartCommand = new RelayCommand(StartKeySpam, CanStartExecute);
+            LoadSettingCommand = new RelayCommand(LoadSetting, CanLoadSettingExecute);
+            StopCommand = new RelayCommand(StopKeySpam, CanStopExecute);
+            runningFlag = true;
+
+
+        }
+
+        private bool CanStopExecute()
+        {
+            return true;
+        }
+
+        private void StopKeySpam()
+        {
+            runningFlag = false;
+        }
+
+        private bool CanStartExecute()
+        {
+            return !string.IsNullOrEmpty(SettingFilePath);
+        }
+
+        private bool CanLoadSettingExecute()
+        {
+            return true;
+        }
+
+        private void LoadSetting()
+        {
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            openFileDialog.Filter = "Tian Files (*.tian)|*.tian|Dino Files (*.dino)|*.dino|3girl Files (*.3girl)|*.3girl|All Files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                SettingFilePath = openFileDialog.FileName;
+            }
+            StreamReader settingFile = new StreamReader(SettingFilePath);
+            string line = "";
+
+            validKeyList = new List<IntPtr>();
+            ProcessName = settingFile.ReadLine();
+            while ((line = settingFile.ReadLine()) != null)
+            {               
+                if (keyDictionary.ContainsKey(line))
+                {
+                    validKeyList.Add(keyDictionary[line]);
+                }
+            }
+            StartCommand.RaiseCanExecuteChanged();
+        }
+
+        private void StartKeySpam()
+        {
+            runningFlag = !runningFlag;
+            //worker = new BackgroundWorker();
+            ////worker.WorkerReportsProgress = true;
+            //worker.WorkerSupportsCancellation = true;
+            //worker.DoWork += keySpamTask;
+            //worker.RunWorkerAsync();          
+            processList = Process.GetProcessesByName(ProcessName);
+            if (processList.Length > 0)
+            {
+                while (runningFlag)
+                {
+                    foreach (Process P in processList)
+                    {
+                        IntPtr edit = P.MainWindowHandle;
+                        foreach (IntPtr validKey in validKeyList)
+                        {
+
+                            PostMessage(edit, WM_KEYDOWN, validKey, IntPtr.Zero);
+                            PostMessage(edit, WM_KEYUP, validKey, IntPtr.Zero);
+                            System.Threading.Thread.Sleep(10);
+
+                        }
+                        ////PostMessage(edit, WM_KEYDOWN, (IntPtr)(Keys.Control), IntPtr.Zero);
+                        //PostMessage(edit, WM_KEYDOWN, (IntPtr)(Keys.A), IntPtr.Zero);
+                        //PostMessage(edit, WM_KEYDOWN, (IntPtr)(Keys.A), IntPtr.Zero);
+                        //PostMessage(edit, WM_KEYDOWN, (IntPtr)(Keys.A), IntPtr.Zero);
+                        //PostMessage(edit, WM_KEYDOWN, (IntPtr)(Keys.Tab), IntPtr.Zero);
+                        //PostMessage(edit, WM_KEYDOWN, (IntPtr)(Keys.A), IntPtr.Zero);
+                        //PostMessage(edit, WM_KEYDOWN, (IntPtr)(Keys.A), IntPtr.Zero);
+                        //PostMessage(edit, WM_KEYDOWN, (IntPtr)(Keys.A), IntPtr.Zero);
+                        ////PostMessage(edit, WM_KEYUP, (IntPtr)(Keys.Control), IntPtr.Zero);
+                    }
+                }
+            }
+        }
+
+        private void keySpamTask(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bgworker = sender as BackgroundWorker;
+
+            processList = Process.GetProcessesByName(ProcessName);
+            if (processList.Length > 0 && bgworker.CancellationPending == false)
+            {
+                while (true)
+                {
+                    foreach (Process P in processList)
+                    {
+                        IntPtr edit = P.MainWindowHandle;
+                        foreach (IntPtr validKey in validKeyList)
+                        {
+
+                            PostMessage(edit, WM_KEYDOWN, validKey, IntPtr.Zero);
+                            PostMessage(edit, WM_KEYUP, validKey, IntPtr.Zero);
+                            System.Threading.Thread.Sleep(10);
+
+                        }
+                        ////PostMessage(edit, WM_KEYDOWN, (IntPtr)(Keys.Control), IntPtr.Zero);
+                        //PostMessage(edit, WM_KEYDOWN, (IntPtr)(Keys.A), IntPtr.Zero);
+                        //PostMessage(edit, WM_KEYDOWN, (IntPtr)(Keys.A), IntPtr.Zero);
+                        //PostMessage(edit, WM_KEYDOWN, (IntPtr)(Keys.A), IntPtr.Zero);
+                        //PostMessage(edit, WM_KEYDOWN, (IntPtr)(Keys.Tab), IntPtr.Zero);
+                        //PostMessage(edit, WM_KEYDOWN, (IntPtr)(Keys.A), IntPtr.Zero);
+                        //PostMessage(edit, WM_KEYDOWN, (IntPtr)(Keys.A), IntPtr.Zero);
+                        //PostMessage(edit, WM_KEYDOWN, (IntPtr)(Keys.A), IntPtr.Zero);
+                        ////PostMessage(edit, WM_KEYUP, (IntPtr)(Keys.Control), IntPtr.Zero);
+                    }
+                }
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+
+}
